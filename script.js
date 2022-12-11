5b2bea64f732ccc2b1a4052d690d6eda3ab4364a
f1redr4g0n <>
2022-12-11 11:00:55 -0300

    added more fake recipes

--- script.js
+++ script.js
@@ -22,9 +22,7 @@ function createRecipeCard(name, category, rating, preparationTime, image) {
   return $recipeCard;
 }
 
-const $recipeCardsContainer =
-    document.querySelector('.js-recipe-cards-container');
-$recipeCardsContainer.append(
+const $todaysHighlights = [
   createRecipeCard(
     'Spaghetti',
     'Pasta',
@@ -33,5 +31,26 @@ $recipeCardsContainer.append(
     'https://images.unsplash.com/photo-1595295333158-4742f28fbd85?ixlib=rb-4.0.\
 3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&\
 q=80'
+  ),
+  createRecipeCard(
+    'Bread',
+    'Pasta',
+    5,
+    '10min',
+    'https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=co\
+mpress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  ),
+  createRecipeCard(
+    'Salad',
+    'Vegetables',
+    5,
+    '20min',
+    'https://images.pexels.com/photos/257816/pexels-photo-257816.jpeg?auto=comp\
+ress&cs=tinysrgb&w=1260&h=750&dpr=1'
   )
-);
+];
+$todaysHighlights.forEach(($todaysHighlight) => {
+  const $todaysHighlightsContainer =
+      document.querySelector('.todays-highlights-container');
+  $todaysHighlightsContainer.append($todaysHighlight);
+});
